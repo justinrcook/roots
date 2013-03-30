cdc9561b0a7ff308bcc65ce7706dd8fb70c180b9
Justin Cook <justinrcook@gmail.com>
2013-03-30 15:12:10 -0500

    Use blog name in the page title.

--- templates/head.php
+++ templates/head.php
@@ -5,7 +5,10 @@
 <!--[if gt IE 8]><!--> <html class="no-js" <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
-  <title><?php wp_title(''); ?></title>
+	<title>
+		<?php wp_title('|',true,'right'); ?>
+		<?php bloginfo('name'); ?>
+	</title>  
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <?php wp_head(); ?>
